e5c9ae1345aab54ac59d25142a4e67a6f951263c
lyskh <allysakhaerepeso@csab.edu.ph>
2024-02-08 02:13:23 -0400

    complete version with all the codes

---
+++ pages/home.php
@@ -0,0 +1,4 @@
+<div class="page-title mt-12 text-center">Attendance Management System</div>
+<div class="mt-7 text-center">Genolos, Repeso</div>
+<div class="mt-7 text-center">with few revisions</div>
+<hr>
